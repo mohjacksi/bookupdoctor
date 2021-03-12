34b8f3b57b0833b7d021980678955c72b60ee7fa
Mohammed Jacksi <moh.jacksi@gmail.com>
2021-03-12 13:54:22 +0300

    .

--- database/seeders/DoctorSpecialtyTableSeeder.php
+++ database/seeders/DoctorSpecialtyTableSeeder.php
@@ -15,7 +15,7 @@ class DoctorSpecialtyTableSeeder extends Seeder
     public function run()
     {
         $num_of_specialites = 2;
-        for ($i = 1; $i <= 100; $i++) {
+        for ($i = 1; $i <= 13778; $i++) {
             Doctor::findOrFail($i)->specialties()->sync($i % $num_of_specialites + 1);
             Doctor::findOrFail($i)->specialties()->sync(($i+2) % $num_of_specialites + 1);
             Doctor::findOrFail($i)->specialties()->sync(($i+8) % $num_of_specialites + 1);
