34a370d305480a6a036a35427c038069971d502e
Mohammed Jacksi <moh.jacksi@gmail.com>
2021-04-24 01:14:52 +0300

    delete soon

--- app/Http/Controllers/Api/V1/Admin/CitiesApiController.php
+++ app/Http/Controllers/Api/V1/Admin/CitiesApiController.php
@@ -17,9 +17,88 @@ class CitiesApiController extends Controller
     {
         // abort_if(Gate::denies('city_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         $for_appointment = request()->for_appointment;
-        if(isset($for_appointment))
-            return new CityResource(City::select(['id','name'])->get());//->where('name', 'not like', "%-%")->get());
-        else
+         if(isset($for_appointment))
+           return '{
+            "data": [
+              {
+                "id": 1,
+                "name": "بغداد"
+              },
+              {
+                "id": 2,
+                "name": "كركوك"
+              },
+              {
+                "id": 3,
+                "name": "الأنبار"
+              },
+              {
+                "id": 4,
+                "name": "البصرة"
+              },
+              {
+                "id": 5,
+                "name": "نينوى"
+              },
+              {
+                "id": 6,
+                "name": "أربيل"
+              },
+              {
+                "id": 7,
+                "name": "حلبجة"
+              },
+              {
+                "id": 8,
+                "name": "النجف"
+              },
+              {
+                "id": 9,
+                "name": "ذي قار"
+              },
+              {
+                "id": 10,
+                "name": "ديالى"
+              },
+              {
+                "id": 11,
+                "name": "المثنى"
+              },
+              {
+                "id": 12,
+                "name": "القادسية"
+              },
+              {
+                "id": 13,
+                "name": "ميسان"
+              },
+              {
+                "id": 14,
+                "name": "واسط"
+              },
+              {
+                "id": 15,
+                "name": "صلاح الدين"
+              },
+              {
+                "id": 16,
+                "name": "دهوك"
+              },
+              {
+                "id": 17,
+                "name": "السليمانية"
+              },
+              {
+                "id": 18,
+                "name": "بابل"
+              },
+              {
+                "id": 19,
+                "name": "كربلاء"
+              }
+            ]
+          }';
+         else
         
             return new CityResource(City::select(['id','name'])->get());
     }
